608b55b6b0ed341eaa0e5b5ee0e288b5a11dc9ba
katerina <sk.katherine@gmail.com>
2020-06-10 20:27:54 +0300

    updated readme

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ API link: [https://developers.google.com/maps/documentation/javascript/direction
 
 ## Instructions
 
+### Get an API key from Google
+
+First of all, to use this, you need a Google Maps API key. You can generate one here: [https://developers.google.com/maps/documentation/javascript/get-api-key](https://developers.google.com/maps/documentation/javascript/get-api-key).
+
+### Add API key to map
+
+Search for `YOUR_API_KEY` in the code, and replace it with the API key you generated from the link above.
+
+### Running the app
+
+I'm using `gulp`. So:
+
 First
 
 ```
--- app/scripts/mapscript.ts
+++ app/scripts/mapscript.ts
@@ -2,18 +2,17 @@ export function loadMapScript(libraries: string, c: Function): void {
   if (!document.getElementById("gmap")) {
     const script = document.createElement("script");
     script.type = "text/javascript";
-    script.src =
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDeG77r1asgi9uVe4S5Y7easEUmb1Vsw4c";
+    script.src = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY";
     if (libraries && libraries.length > 0) {
       script.src = script.src + "&libraries=" + libraries;
     }
     script.id = "gmap";
     script.addEventListener(
       "load",
-      e => {
+      (e) => {
         c(e);
       },
-      false
+      false,
     );
     document.body.appendChild(script);
   }
